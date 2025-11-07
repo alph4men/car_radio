2e2d32e8d5ba12686b6fe3228a956479f05d9942
alph4men <138143455+alph4men@users.noreply.github.com>
2025-11-07 22:24:07 +0100

    Fix YouTube playback error by removing forced origin

--- car_radio/lua/car_radio/cl_car_radio.lua
+++ car_radio/lua/car_radio/cl_car_radio.lua
@@ -123,15 +123,12 @@ body.unlocked #overlay{display:none;}
     holder.appendChild(node);
     return node;
   }
-  var origin = 'https://www.youtube.com';
-
   function ensurePlayer(id){
     if(players[id]) return players[id];
     var node=ensureContainer(id);
     var player=new YT.Player(node.id,{
       width:'320',height:'180',
-      host:origin,
-      playerVars:{autoplay:1,controls:0,disablekb:1,fs:0,rel:0,iv_load_policy:3,modestbranding:1,playsinline:1,enablejsapi:1,origin:origin},
+      playerVars:{autoplay:1,controls:0,disablekb:1,fs:0,rel:0,iv_load_policy:3,modestbranding:1,playsinline:1,enablejsapi:1},
       events:{
         onStateChange:function(evt){
           if(evt && evt.data===0 && window.gmod && gmod.VideoEnded){ try{ gmod.VideoEnded(id); }catch(e){} }
